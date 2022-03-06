84a27404f651d032b1794347a561cd501dd8f8bd
Rayy1111 <101046530+Rayy1111@users.noreply.github.com>
2022-03-07 00:11:58 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // METAMASK CONNECTION
 const TIMEOUT = 1000;
-const COLLECTION_NAME = 'CodeCats';
+const COLLECTION_NAME = '韭菜';
 let editions = [];
 let dots = 1;
 
@@ -78,15 +78,15 @@ function updateStatusText(isOwner, checking) {
   const statusText = document.querySelector('.owner-status');
   if(checking) {
     if(isOwner) {
-      statusText.innerText = `You do own ${COLLECTION_NAME}!! 😻 Let's see how many${renderDots(dots)}`;
+      statusText.innerText = `You do own ${COLLECTION_NAME}!! 🥬 Let's see how many${renderDots(dots)}`;
     } else {
-      statusText.innerText = `Checking to see if you own any ${COLLECTION_NAME} 😻${renderDots(dots)}`;
+      statusText.innerText = `恭喜你 成為韭菜的一員!! ${COLLECTION_NAME} 🥬${renderDots(dots)}`;
     }
   } else {
     if(isOwner) {
-      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 😻`;
+      statusText.innerText = `You own ${editions.length} ${COLLECTION_NAME}!! 🥬`;
     } else {
-      statusText.innerText = `You don't own any ${COLLECTION_NAME} 😿`;
+      statusText.innerText = `你還不是韭菜!! 快過來給我割 ${COLLECTION_NAME} 🥬`;
     }
   }
   dots = dots === 3 ? 1 : dots + 1;
